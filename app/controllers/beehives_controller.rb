623ee9b738581fea2de73e3939000c833c1c4c3e
Branden <brandenehman45@gmail.com>
2021-11-08 16:37:51 -0700

    Got my POST request to work

--- app/controllers/beehives_controller.rb
+++ app/controllers/beehives_controller.rb
@@ -4,4 +4,14 @@ class BeehivesController < ApplicationController
         Beehive.all.to_json(:include => :owner, :except => :owner_id)
     end
 
+    post "/beehives" do
+        owner = Owner.find_or_create_by(first_name: params[:owner_first_name], last_name: params[:owner_last_name])
+        beehive = Beehive.new(queen_name: params[:queen_name], type_of_bees: params[:type_of_bees], number_of_boxes: params[:number_of_boxes], owner_id: owner.id)
+        if beehive.save
+            beehive.to_json(:include => :owner, :except => :owner_id)
+        else
+            tracks.errors.to_json
+        end
+    end
+
 end
